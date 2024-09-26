6dbf42330027ad5b5ff50004c6a937d30382b763
GG-Balder <154797555+GG-Balder@users.noreply.github.com>
2024-09-26 18:24:16 +0800

    fix: withdrawn bear background

--- src/sections/bridge/index.tsx
+++ src/sections/bridge/index.tsx
@@ -1,8 +1,10 @@
+import BearBackground from "@/components/bear-background"
 import Card from "@/components/card"
 
 export default function Bridge() {
-    return <div className="w-[520px] m-auto">
-        <div className="text-[60px] text-center py-[30px]">Bridge</div>
-        <Card>2222</Card>
-    </div>
+    return (
+        <BearBackground>
+            <div>Bridge</div>
+        </BearBackground>
+    )
 }
