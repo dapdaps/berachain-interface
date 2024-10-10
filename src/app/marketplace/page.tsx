a140f97bf15dc6a52a0d231da2d1e16e61ad31d3
Emily <emily@ref.finance>
2024-10-10 10:17:37 +0800

    add pools.

--- src/app/marketplace/page.tsx
+++ src/app/marketplace/page.tsx
@@ -1,31 +1,7 @@
 import MarketPlaceView from '@/sections/marketplace';
-import AddLiquidity from '@/sections/liquidity/remove-liquidity-modal';
 
 const MarketPlace = () => {
-  return (
-    <AddLiquidity
-      dex='kodiak'
-      version='v3'
-      fee={3000}
-      token0={{
-        address: 'native',
-        isNative: true,
-        chainId: 80084,
-        symbol: 'BERA',
-        decimals: 18,
-        name: 'BERA',
-        icon: '/assets/tokens/bera.svg'
-      }}
-      token1={{
-        address: '0x0E4aaF1351de4c0264C5c7056Ef3777b41BD8e03',
-        chainId: 80084,
-        symbol: 'HONEY',
-        decimals: 18,
-        name: 'HONEY',
-        icon: '/images/dapps/honey.png'
-      }}
-    />
-  );
+  return <MarketPlaceView />;
 };
 
 export default MarketPlace;
--- src/sections/swap/index.tsx
+++ src/sections/swap/index.tsx
@@ -3,9 +3,15 @@ import Content from './Content';
 
 export default function Swap({ dapp }: any) {
   return (
-    <div className='relative w-[520px] mx-[auto] pt-[110px]'>
+    <div className='relative w-[520px] pt-[30px]'>
       <Content dapp={dapp} />
-      <DappIcon src={dapp.icon} alt={dapp.name} name={dapp.name} type='swap' />
+      <DappIcon
+        src={dapp.icon}
+        alt={dapp.name}
+        name={dapp.name}
+        type='swap'
+        style={{ top: -76 }}
+      />
     </div>
   );
 }
