41a6be744bdd455798047f35767fd5c56dd56a3c
ben <benjm240@gmail.com>
2024-10-25 19:45:17 +0800

    add cave modal

---
+++ src/sections/cave/HatHover.tsx
@@ -0,0 +1,40 @@
+import { useRef, useState } from "react";
+import TipModal from "./TipModal";
+import useTipModal from "./useTipModal";
+
+const data = {
+    tip: 'Bridge',
+    title: 'How to Unlock Hats?',
+    list: [{
+        icon: '/images/cave/hat/hat-1-1.png',
+        content: '$100+ per transaction, complete at least 1 transaction',
+    }, {
+        icon: '/images/cave/hat/hat-2-2.png',
+        content: '$100+ per transaction, complete at least 10 transaction',
+    }, {
+        icon: '/images/cave/hat/hat-3-3.png',
+        content: '$100+ per transaction, complete at least 100 transaction',
+    }, {
+        icon: '/images/cave/hat/hat-4-4.png',
+        content: '$100+ per transaction, complete at least 1000 transaction',
+    }]
+}
+
+export default function HatHover() {
+    const boxRef = useRef<any>(null)
+    const {showTip, setTipShow} = useTipModal(boxRef)
+    const [showSelf, setShowSelf] = useState(false)
+
+    return <div ref={boxRef} 
+    onMouseEnter={() => { setShowSelf(true) }} 
+    onMouseLeave={() => { setShowSelf(false); setTipShow(false); }}  
+    style={{ opacity: showSelf ? 1: 0 }} 
+    onClick={() => {
+        setTipShow(true)
+    }} className="absolute cursor-pointer w-[563px] h-[100px] left-[50%] top-[270px] translate-x-[-50%] bg-[#EBF4794D] border-[#EBF479] border-[3px] rounded-[20px]">
+        {
+            showTip && <TipModal data={data} style={{ left: '50%', transform: 'translateX(-50%)', top: '80%' }}/>
+        }
+    </div>
+
+}
---
+++ src/sections/cave/TipModal.tsx
@@ -0,0 +1,38 @@
+interface Props {
+    data: TipData;
+    style?: any;
+    iconWidth?: number;
+    note?: React.ReactNode;
+}
+
+type TipData = {
+    tip: string;
+    title: string;
+    list: ListItemData[];
+}
+
+type ListItemData = {
+    icon: string;
+    content: string | React.ReactNode;
+}
+
+export default function TipModal({ data, style, note, iconWidth = 97 }: Props) {
+    return <div style={style} className=" shadow-[10px_10px_0px_0px_#00000040] absolute bg-[#FFFDEB] border border-[#000] rounded-[20px] w-[396px] p-[25px]">
+        <div className="w-[131px] h-[40px] absolute right-[20px] text-center top-[-7px] font-bold leading-[100%] flex justify-center items-center text-[16px] bg-[url('/images/cave/tip-top.png')] bg-contain bg-no-repeat bg-bottom">{ data.tip }</div>
+        <div className="text-[20px] font-CherryBomb">{data.title}</div>
+        <div className="mt-[10px]">
+            {
+                data.list.map(item => {
+                    return <div key={item.icon} className="flex items-center mt-[10px] bg-[#fff] border border-[#000] rounded-[12px] py-[5px] pr-[32px]">
+                        <div style={{ width: iconWidth }} className="mr-[10px] ml-[-15px] h-[42px] w-[97px] relative">
+                            <img src={item.icon} className="h-[53px] absolute left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%]" />
+                        </div>
+                        <div className="text-[14px] font-medium">{item.content}</div>
+                    </div>
+                })
+            }
+        </div>
+
+        <div className="mt-[20px]">{ note && note }</div>
+    </div>
+}
