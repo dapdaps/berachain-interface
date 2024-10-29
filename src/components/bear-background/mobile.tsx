4108b1b7b75e1d3b7be9dea17548943b8c67f1ea
Emily <emily@ref.finance>
2024-10-29 20:07:15 +0800

    fix background component.

--- src/components/bear-background/mobile.tsx
+++ src/components/bear-background/mobile.tsx
@@ -2,7 +2,7 @@ import { Clouds } from './clouds';
 
 export default function Mobile({ children }: any) {
   return (
-    <div className='relative hidden md:block min-w-full	min-h-screen'>
+    <div className='relative hidden md:block min-w-full'>
       <Clouds />
       <div className='relative z-10'>{children}</div>
     </div>
