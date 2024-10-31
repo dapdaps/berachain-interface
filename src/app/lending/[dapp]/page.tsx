5db6a6b217ca11fb82c126e2648c1273977b5370
jimmygu <jimmyguu2024@gmail.com>
2024-10-31 17:34:28 +0800

    feat: add lending mobile styles

--- src/app/lending/[dapp]/page.tsx
+++ src/app/lending/[dapp]/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import BearBackground from '@/components/bear-background/laptop';
+import BearBackground from '@/components/bear-background';
 import { useParams } from 'next/navigation';
 import PageBack from '@/components/back';
 import dynamic from 'next/dynamic';
 import useTokenPrice from '@/hooks/use-token-price';
 import { useEffect } from 'react';
 import useClickTracking from '@/hooks/use-click-tracking';
+import useIsMobile from '@/hooks/use-isMobile';
 
 const Dolomite = dynamic(() => import('@/sections/Lending/Dolomite'));
 const Bend = dynamic(() => import('@/sections/Lending/Bend'));
@@ -15,6 +16,7 @@ export default function LendingDAppPage() {
   const { dapp } = useParams();
   const { initializePrice } = useTokenPrice();
   const { handleReport } = useClickTracking();
+  const isMobile = useIsMobile();
 
   useEffect(() => {
     switch (dapp) {
@@ -34,7 +36,11 @@ export default function LendingDAppPage() {
   return (
     <BearBackground type='dapp'>
       <div className='p-[25px_35px]'>
-        <PageBack />
+        {
+          !isMobile && (
+            <PageBack />
+          )
+        }
         <div className=''>
           {dapp === 'dolomite' && <Dolomite />}
           {dapp === 'bend' && <Bend />}
