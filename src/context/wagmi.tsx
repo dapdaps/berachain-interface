873632185b895ea6cd3fdd0acd30d457aa5e82b0
jimmygu <jimmyguu2024@gmail.com>
2024-11-04 18:48:11 +0800

    feat: Coinbase wallet supported

--- src/context/wagmi.tsx
+++ src/context/wagmi.tsx
@@ -36,10 +36,12 @@ const modal = createAppKit({
   metadata: metadata,
   featuredWalletIds: [
     'BeraSig',
-    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
-    '971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709',
-    '38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662',
-    '8a0ee50d1f22f6651afcae7eb4253e52a3310b90af5daef78a8c4929a9bb99d4',
+    // Explorer: https://explorer.walletconnect.com/
+    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', // Metamask
+    'fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa', // Coinbase
+    '971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709', // OKX
+    '38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662', // Bitget
+    '8a0ee50d1f22f6651afcae7eb4253e52a3310b90af5daef78a8c4929a9bb99d4', // Binance
   ],
   customWallets,
   features: {
