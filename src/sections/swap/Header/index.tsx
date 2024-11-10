d51d300926ec69408d1d97e60e4d465fd500c89d
Emily <emily@ref.finance>
2024-11-10 12:46:36 +0800

    add bridge hints on get token modal.

--- src/sections/swap/Header/index.tsx
+++ src/sections/swap/Header/index.tsx
@@ -1,9 +1,12 @@
-import Setting from './Setting';
+import Setting from "./Setting";
 
-export default function Header({ showSetting, className, style }: any) {
+export default function Header({ showSetting, className, style, title }: any) {
   return (
-    <div className={`h-[50px] flex justify-end items-center mt-[-20px] md:justify-between md:pt-[18px] md:pb-[12px] ${className}`} style={style}>
-      <div className='text-[18px] font-bold	md:block'>Swap</div>
+    <div
+      className={`h-[50px] flex justify-end items-center mt-[-20px] md:justify-between md:pt-[18px] md:pb-[12px] ${className}`}
+      style={style}
+    >
+      <div className="text-[18px] font-bold	md:block">{title || "Swap"}</div>
       {showSetting && <Setting />}
     </div>
   );
