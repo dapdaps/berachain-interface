85b7b0c24b9d8dbbf666316a9573fdcc83f201f2
Yyy <snwyz1st@gmail.com>
2024-11-13 17:36:56 +0800

    fix domain

--- src/sections/dapps/index.tsx
+++ src/sections/dapps/index.tsx
@@ -138,14 +138,10 @@ const DAppsView = () => {
 
   return (
     <div
-      className="md:bg-[#96d6ff] h-full md:mb-[70px] relative"
-      style={{
-        height: "calc(100dvh)"
-      }}
-    >
+      className="md:bg-[#96d6ff] h-full md:mb-[70px] relative md:h-[calc(100dvh)]">
       <PageBack className="absolute left-[12px] top-[17px]" />
       <PageTitle className="pt-[30px] mb-[75px]">dApps</PageTitle>
-      <div className={clsx('absolute bottom-[143px] md:bottom-[200px] left-1/2 -translate-x-1/2 md:scale-[0.76] md:z-[1]', visibleHeight < 750 ? 'md:bottom-[70px] md:scale-[0.74]' : '' )}>
+      <div className={clsx('absolute lg:bottom-[230px] md:bottom-[200px] left-1/2 -translate-x-1/2 md:scale-[0.76] md:z-[1]', visibleHeight < 750 ? 'md:bottom-[70px] md:scale-[0.74]' : '' )}>
         <div className="w-[95px] h-[415px] bg-[#906925] border-black border-[2px] relative">
           <IconLeftLeaf className="absolute left-[-25px] bottom-[-10px]" />
           <IconRightLeaf className="absolute right-[-10px] bottom-[-10px]" />
