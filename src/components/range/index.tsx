cb216693ebcefcb1a202dd1230ff5c5f0f4dfa8a
Emily <emily@ref.finance>
2024-11-15 16:23:33 +0800

    fix range style.

--- src/components/range/index.tsx
+++ src/components/range/index.tsx
@@ -1,6 +1,7 @@
+import clsx from "clsx";
 import styled from "styled-components";
 
-const StyledInputRange = styled.div`
+const StyledInputRange = styled.div<{ $position: string }>`
   margin-top: 24px;
   input[type="range"] {
     display: block;
@@ -13,21 +14,44 @@ const StyledInputRange = styled.div`
     margin: 0 auto;
     outline: none;
   }
+
   input[type="range"]::-webkit-slider-thumb {
     -webkit-appearance: none;
-    background-color: #ffdc50;
-    border: 1px solid #000;
+    background-color: transparent;
     width: 22px;
     height: 22px;
     border-radius: 50%;
-    cursor: url("../../public/images/cursor.svg") 12 0;
+    position: relative;
+    z-index: 5;
   }
 `;
 
 export default function Range({ value, onChange, style, className }: any) {
   return (
-    <StyledInputRange style={style} className={className}>
-      <input type="range" value={value} onChange={onChange} />
+    <StyledInputRange
+      $position={value === 100 ? "0" : value === 0 ? "16" : "-16"}
+      style={style}
+      className={clsx("relative", className)}
+    >
+      <input
+        type="range"
+        value={value}
+        onChange={onChange}
+        className="appearance-none"
+      />
+      <div
+        className={clsx(
+          "absolute top-0 left-0 h-[8px] rounded-[16px] bg-[#ffdc50]"
+        )}
+        style={{ width: value + "%" }}
+      >
+        <div
+          className={clsx(
+            "absolute border border-black w-[22px] h-[22px] rounded-full top-[-7px] cursor-pointer bg-[#ffdc50]",
+            Number(value) === 0 ? "right-[-16px]" : "right-0"
+          )}
+        />
+      </div>
     </StyledInputRange>
   );
 }
