0ec99306b2563c28cec6dd934a53bd9d980e1153
Emily <emily@ref.finance>
2024-11-26 10:30:05 +0800

    add bgt address.

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,10 +1,12 @@
-import { beraB } from './tokens/bera-bArtio';
+import { beraB } from "./tokens/bera-bArtio";
 
 export const DEFAULT_CHAIN_ID = 80084;
 
-export const DEFAULT_SWAP_DAPP = 'bex';
+export const DEFAULT_SWAP_DAPP = "bex";
 
-export const DEFAULT_LIQUIDITY_DAPP = 'infrared';
+export const DEFAULT_LIQUIDITY_DAPP = "infrared";
+
+export const BGT_ADDRESS = "0xbDa130737BDd9618301681329bF2e46A016ff9Ad";
 
 export const TOKENS: Record<string, any> = Object.values(beraB).reduce(
   (acc, curr) => ({ ...acc, [curr.address.toLowerCase()]: curr }),
