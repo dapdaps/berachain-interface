9bd6134a21315bc845234da35a97a20849e7e9cc
jimmygu <jimmyguu2024@gmail.com>
2024-12-02 15:09:26 +0800

    feat: add Beramas entry to map

--- src/hooks/use-christmas.ts
+++ src/hooks/use-christmas.ts
@@ -1,11 +1,17 @@
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 import { SceneContext } from '@/context/scene';
+import { SceneStatus } from '@/configs/scene';
 
 export function useChristmas() {
   const context = useContext(SceneContext);
   const scene = context.scene.current;
 
+  const isChristmas = useMemo(() => {
+    return scene?.status === SceneStatus.Ongoing;
+  }, [scene]);
+
   return {
     ...scene,
+    isChristmas,
   };
 }
--- src/sections/home/map-modal.tsx
+++ src/sections/home/map-modal.tsx
@@ -13,7 +13,6 @@ import ReactDOM from 'react-dom';
 import IconClose from '@public/images/modal/close.svg';
 import useClickTracking from '@/hooks/use-click-tracking';
 import { useChristmas } from '@/hooks/use-christmas';
-import { SceneStatus } from '@/configs/scene';
 
 const dAppClipPath =
   'M208.33 2.96745L11.5462 105.957C4.52793 109.63 0.338879 117.099 0.864137 125.003L9.08074 248.644C9.84853 260.197 20.2364 268.686 31.7113 267.138L216.435 242.211C217.477 242.071 218.506 241.848 219.512 241.546L391.686 189.845C393.552 189.285 395.49 189 397.438 189H500.561C507.102 189 513.229 185.802 516.969 180.436L537.25 151.337C542.176 144.269 542.023 134.841 536.87 127.938L472.504 41.7046C469.303 37.415 464.515 34.5878 459.212 33.8557L220.339 0.875262C216.216 0.305971 212.018 1.03735 208.33 2.96745Z';
@@ -205,7 +204,7 @@ const MapModal = () => {
   const [isClient, setIsClient] = useState(false);
 
   const entries = useMemo(() => {
-    if (christmas?.status !== SceneStatus.Ongoing) {
+    if (christmas?.isChristmas) {
       return PartList;
     }
     return [...PartList, {
