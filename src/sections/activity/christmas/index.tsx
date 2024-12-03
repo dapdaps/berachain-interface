2d4055586fbdbeb78df47dff0b2610c74381c088
jimmygu <jimmyguu2024@gmail.com>
2024-12-03 15:54:17 +0800

    feat: add christmas summary page

--- src/sections/activity/christmas/index.tsx
+++ src/sections/activity/christmas/index.tsx
@@ -1,9 +1,15 @@
 import Top from "@/sections/activity/christmas/sections/top";
+import Summary from '@/sections/activity/christmas/sections/summary';
+import GiftBox from '@/sections/activity/christmas/sections/gift-box';
+import Quest from '@/sections/activity/christmas/sections/quest';
 
 export default function Christmas() {
   return (
     <div className="">
       <Top />
+      <Summary />
+      <GiftBox />
+      <Quest />
     </div>
   );
 }
--- src/sections/activity/christmas/sections/top.tsx
+++ src/sections/activity/christmas/sections/top.tsx
@@ -44,31 +44,31 @@ const Top = () => {
               style={{ animationDelay: '0', animationDuration: '8s' }}
             />
             <img
-              src="/images/activity/christmas/start-left-top.svg"
+              src="/images/activity/christmas/star-left-top.svg"
               alt=""
               className="absolute left-0 top-0 animate-blink"
               style={{ animationDelay: '0', animationDuration: '7s' }}
             />
             <img
-              src="/images/activity/christmas/start-left-bot.svg"
+              src="/images/activity/christmas/star-left-bot.svg"
               alt=""
               className="absolute left-[80px] bottom-[55px] animate-blink"
               style={{ animationDelay: '2s', animationDuration: '3s' }}
             />
             <img
-              src="/images/activity/christmas/start-center-top.svg"
+              src="/images/activity/christmas/star-center-top.svg"
               alt=""
               className="absolute left-[270px] bottom-[155px] animate-blink"
               style={{ animationDelay: '0', animationDuration: '10s' }}
             />
             <img
-              src="/images/activity/christmas/start-right-top.svg"
+              src="/images/activity/christmas/star-right-top.svg"
               alt=""
               className="absolute right-[150px] bottom-[120px] animate-blink"
               style={{ animationDelay: '1s', animationDuration: '8s' }}
             />
             <img
-              src="/images/activity/christmas/start-right-bot.svg"
+              src="/images/activity/christmas/star-right-bot.svg"
               alt=""
               className="absolute right-[0px] bottom-[10px] animate-blink"
               style={{ animationDelay: '5s', animationDuration: '5s' }}
