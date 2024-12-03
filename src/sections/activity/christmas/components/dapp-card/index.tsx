6a40767b343595a10b7b5886205286d6ef5c844f
Emily <emily@ref.finance>
2024-12-03 18:28:13 +0800

    add project card.

--- src/sections/activity/christmas/components/dapp-card/index.tsx
+++ src/sections/activity/christmas/components/dapp-card/index.tsx
@@ -1,7 +1,7 @@
 import CheckButton from "../check-button";
 import Button from "./button";
 
-export default function TaskCard() {
+export default function DappCard() {
   return (
     <div className="w-[400px] h-[260px] flex flex-col justify-between p-[20px] rounded-[20px] border border-black bg-[#B5956E] shadow-[-20px_26px_60px_0px_rgba(0, 0, 0, 0.20)_inset]">
       <div>
