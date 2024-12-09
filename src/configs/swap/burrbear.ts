69528b35911d395205c5a67883c78e284325a2b6
Emily <emily@ref.finance>
2024-12-09 11:03:00 +0800

    add lbgt.

---
+++ src/configs/swap/burrbear.ts
@@ -0,0 +1,10 @@
+import { beraB } from "../tokens/bera-bArtio";
+
+export default {
+  name: "Burrbear",
+  icon: "",
+  defaultInputCurrency: beraB["bera"],
+  tokens: {
+    80084: [beraB["bera"], beraB["lbgt"]]
+  }
+};
