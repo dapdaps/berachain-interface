f8564bb7c6ce16292bc2879943ccfa8623ce9696
Emily <emily@ref.finance>
2024-12-09 14:12:50 +0800

    add rules.

--- src/sections/activity/christmas/index.tsx
+++ src/sections/activity/christmas/index.tsx
@@ -4,18 +4,47 @@ import Summary from "@/sections/activity/christmas/sections/summary";
 import GiftBox from "@/sections/activity/christmas/sections/gift-box";
 import Quest from "@/sections/activity/christmas/sections/quest";
 import SceneContextProvider from "@/sections/activity/christmas/context";
+import RulesModal from "./rules-modal";
+import TotalPrizeModal from "./total-prize-modal";
+import { useState } from "react";
 
 export default function Christmas() {
+  const [showRulesModal, setShowRulesModal] = useState(false);
+  const [showTotalPrizeModal, setShowTotalPrizeModal] = useState(false);
   return (
     <SceneContextProvider>
       <div className="">
-        <Top>
+        <Top
+          onOpenRules={() => {
+            setShowRulesModal(true);
+          }}
+        >
           <NFTProgress />
         </Top>
-        <Summary />
+        <Summary
+          onOpenRewards={() => {
+            setShowTotalPrizeModal(true);
+          }}
+        />
         <GiftBox />
         <Quest />
       </div>
+
+      <RulesModal
+        open={showRulesModal}
+        onClose={() => {
+          setShowRulesModal(false);
+        }}
+        onOpenRewards={() => {
+          setShowTotalPrizeModal(true);
+        }}
+      />
+      <TotalPrizeModal
+        open={showTotalPrizeModal}
+        onClose={() => {
+          setShowTotalPrizeModal(false);
+        }}
+      />
     </SceneContextProvider>
   );
 }
--- src/sections/activity/christmas/sections/top.tsx
+++ src/sections/activity/christmas/sections/top.tsx
@@ -1,7 +1,7 @@
-import Moon from '@/sections/activity/christmas/components/moon';
+import Moon from "@/sections/activity/christmas/components/moon";
 
 const Top = (props: any) => {
-  const { children } = props;
+  const { children, onOpenRules } = props;
 
   return (
     <div className="relative">
@@ -11,48 +11,61 @@ const Top = (props: any) => {
       <div className="pt-[35px] w-full h-[418px] bg-[url('/images/activity/christmas/bg-cloud.svg')] bg-no-repeat bg-cover bg-top">
         <div className="flex flex-col items-center gap-[0px]">
           <div className="relative w-[791px] h-[254px]">
-            <img src="/images/activity/christmas/title.svg" alt="" width={791} height={254} />
+            <button
+              onClick={() => {
+                onOpenRules();
+              }}
+              className="absolute right-[90px] top-[104px] w-[68px] h-[32px] rounded-[20px] border border-black bg-[#B5956E] text-[#FFF5A9] shadow-[-20px_26px_60px_0px_rgba(0, 0, 0, 0.20)_inset]"
+            >
+              Rules
+            </button>
+            <img
+              src="/images/activity/christmas/title.svg"
+              alt=""
+              width={791}
+              height={254}
+            />
             <img
               src="/images/activity/christmas/title-circle-left.svg"
               alt=""
               className="absolute left-[240px] bottom-[100px] animate-blink"
-              style={{ animationDelay: '2s', animationDuration: '5s' }}
+              style={{ animationDelay: "2s", animationDuration: "5s" }}
             />
             <img
               src="/images/activity/christmas/title-circle-right.svg"
               alt=""
               className="absolute right-[247px] bottom-[85px] animate-blink"
-              style={{ animationDelay: '0', animationDuration: '8s' }}
+              style={{ animationDelay: "0", animationDuration: "8s" }}
             />
             <img
               src="/images/activity/christmas/star-left-top.svg"
               alt=""
               className="absolute left-0 top-0 animate-blink"
-              style={{ animationDelay: '0', animationDuration: '7s' }}
+              style={{ animationDelay: "0", animationDuration: "7s" }}
             />
             <img
               src="/images/activity/christmas/star-left-bot.svg"
               alt=""
               className="absolute left-[80px] bottom-[55px] animate-blink"
-              style={{ animationDelay: '2s', animationDuration: '3s' }}
+              style={{ animationDelay: "2s", animationDuration: "3s" }}
             />
             <img
               src="/images/activity/christmas/star-center-top.svg"
               alt=""
               className="absolute left-[270px] bottom-[155px] animate-blink"
-              style={{ animationDelay: '0', animationDuration: '10s' }}
+              style={{ animationDelay: "0", animationDuration: "10s" }}
             />
             <img
               src="/images/activity/christmas/star-right-top.svg"
               alt=""
               className="absolute right-[150px] bottom-[120px] animate-blink"
-              style={{ animationDelay: '1s', animationDuration: '8s' }}
+              style={{ animationDelay: "1s", animationDuration: "8s" }}
             />
             <img
               src="/images/activity/christmas/star-right-bot.svg"
               alt=""
               className="absolute right-[0px] bottom-[10px] animate-blink"
-              style={{ animationDelay: '5s', animationDuration: '5s' }}
+              style={{ animationDelay: "5s", animationDuration: "5s" }}
             />
           </div>
         </div>
