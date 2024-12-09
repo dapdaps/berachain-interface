1435898050e7e8a4ca61379669af309ccca684fd
jimmygu <jimmyguu2024@gmail.com>
2024-12-09 11:17:09 +0800

    fix: The homepage icon hover does not provide a description, and the click popup does not open the blind box.

--- src/sections/activity/christmas/enterance.tsx
+++ src/sections/activity/christmas/enterance.tsx
@@ -6,6 +6,9 @@ import IconReload from "@public/images/home/christmas/icon-reload.svg";
 import useHomepageData from "./hooks/use-homepage-data";
 import clsx from "clsx";
 import BoxModal from "./box-modal";
+import Popover, { PopoverPlacement, PopoverTrigger } from '@/components/popover';
+import Card from '@/components/card';
+import Big from 'big.js';
 export default function Enterance({ path }: any) {
   const router = useRouter();
   const { loading, data, onQuery } = useHomepageData();
@@ -15,6 +18,12 @@ export default function Enterance({ path }: any) {
     () => (data?.total_box || 0) - (data?.used_box || 0),
     [data]
   );
+
+  const handleOpen = (e: any) => {
+    if (Big(remainBox || 0).lte(0)) return;
+    e.stopPropagation();
+    setOpenModal(true);
+  };
 
   useEffect(() => {
     if (remainBox > 0) {
@@ -34,9 +43,24 @@ export default function Enterance({ path }: any) {
           <BearSnow />
         </div>
         <div className="absolute left-[0px] bottom-[36px] z-[2] w-[139px] h-[58px] rounded-[29px]">
-          <div className="absolute left-[27px] bottom-[23px] z-[1] animate-shake">
-            <GiftBox />
-          </div>
+          <Popover
+            trigger={PopoverTrigger.Hover}
+            placement={PopoverPlacement.TopRight}
+            offset={40}
+            contentClassName="!z-[12]"
+            content={Big(remainBox || 0).gt(0) && (
+              <Card className="w-[173px] !p-[8px_4px_10px_16px]">
+                You’ve got {remainBox} Unopened boxes, {data?.total_box} boxes in total. Click to open.
+              </Card>
+            )}
+          >
+            <div
+              className="absolute left-[27px] bottom-[23px] z-[1] animate-shake"
+              onClick={handleOpen}
+            >
+              <GiftBox />
+            </div>
+          </Popover>
 
           <div className="absolute z-[2] left-[17px] bottom-[7px] h-[30px] rounded-[15px] p-[2px] bg-white">
             <div className="w-full h-full bg-[#C8D060] rounded-[13px] border-[2px] border-black pb-[3px]">
