e3c556ee9445624f4277dda805ea07ceb64f5178
GG-Balder <154797555+GG-Balder@users.noreply.github.com>
2024-12-20 10:30:17 +0800

    fix: remove bg

--- src/components/bear-background/components/move-bg.tsx
+++ src/components/bear-background/components/move-bg.tsx
@@ -28,7 +28,7 @@ const MoveBg = (props: any) => {
         }}
       />
 
-      <div className="flex items-center absolute z-10 left-0 bottom-[197px] h-[250px] animate-slide-to-left"
+      {/* <div className="flex items-center absolute z-10 left-0 bottom-[197px] h-[250px] animate-slide-to-left"
         style={{
           left: -(width + window.screen.availWidth),
           animationDuration: '40s',
@@ -48,17 +48,7 @@ const MoveBg = (props: any) => {
             )
           })
         }
-      </div>
-
-      {/* <div
-        className="absolute z-10 left-0 bottom-[197px] h-[250px] bg-repeat-x bg-left animate-slide-to-left bg-contain"
-        style={{
-          left: -(width * repeat) / 2,
-          animationDuration: '20s',
-          width: width * repeat,
-          backgroundImage: `url("${peoples}")`,
-        }}
-      /> */}
+      </div> */}
     </>
   );
 };
