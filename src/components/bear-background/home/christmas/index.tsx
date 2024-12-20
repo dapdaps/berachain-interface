0bbe9e51b91af69d71ec0fef25f5428789ad1ea1
GG-Balder <154797555+GG-Balder@users.noreply.github.com>
2024-12-20 16:36:10 +0800

    fix: fix big screen bugs

--- src/components/bear-background/home/christmas/index.tsx
+++ src/components/bear-background/home/christmas/index.tsx
@@ -2,13 +2,17 @@ import Bear from '@/components/bear-background/components/bear';
 import MoveBg from '@/components/bear-background/components/move-bg';
 import BeraTown from '@/components/bear-background/components/bera-town';
 import Snow from '@/components/bear-background/home/christmas/snow';
+import { useSize } from 'ahooks';
+import { useMemo } from 'react';
 
 const BeraBgHomeChristmas = () => {
+  const size = useSize(window.document.getElementsByTagName('body')[0]);
+  const isBigScreen = useMemo(() => size?.width > 1920, [size])
   return (
     <>
       <MoveBg
         width={1544}
-        repeat={6}
+        repeat={isBigScreen ? 3 : 6}
         foreground="/images/background/christmas/ground.svg"
         background="/images/background/christmas/trees.svg"
         peoples="/images/background/christmas/peoples.png"
