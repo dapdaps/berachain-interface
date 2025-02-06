81eae219a9f7ddaf8739c80464a030e5cb06a281
Emily <emily@ref.finance>
2025-02-06 23:37:36 +0800

    fix ts.

--- src/hooks/use-bgt.ts
+++ src/hooks/use-bgt.ts
@@ -1,286 +1,301 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from "react";
 
-import multicallAddresses from '@/configs/contract/multicall';
-import useClickTracking from '@/hooks/use-click-tracking';
-import useIsMobile from '@/hooks/use-isMobile';
-import useToast from '@/hooks/use-toast';
-import useInfraredList from '@/sections/staking/hooks/use-infrared-list';
-import { asyncFetch } from '@/utils/http';
-import { multicall } from '@/utils/multicall';
-import Big from 'big.js';
-import { ethers } from 'ethers';
-import _ from 'lodash';
-import { useRouter } from 'next/navigation';
-import useCustomAccount from './use-account';
-import { numberFormatter } from '@/utils/number-formatter';
+import multicallAddresses from "@/configs/contract/multicall";
+import useClickTracking from "@/hooks/use-click-tracking";
+import useIsMobile from "@/hooks/use-isMobile";
+import useToast from "@/hooks/use-toast";
+import useInfraredList from "@/sections/staking/hooks/use-infrared-list";
+import { asyncFetch } from "@/utils/http";
+import { multicall } from "@/utils/multicall";
+import Big from "big.js";
+import { ethers } from "ethers";
+import _ from "lodash";
+import { useRouter } from "next/navigation";
+import useCustomAccount from "./use-account";
+import { numberFormatter } from "@/utils/number-formatter";
+import { DEFAULT_CHAIN_ID } from "@/configs";
 
-
-export const BGT_ADDRESS = "0xbDa130737BDd9618301681329bF2e46A016ff9Ad"
-export const ERC20_ABI = [{
-  "inputs": [],
-  "name": "symbol",
-  "outputs": [
-    {
-      "internalType": "string",
-      "name": "",
-      "type": "string"
-    }
-  ],
-  "stateMutability": "pure",
-  "type": "function"
-}]
-export const ABI = [{
-  "inputs": [
-    {
-      "internalType": "address",
-      "name": "account",
-      "type": "address"
-    }
-  ],
-  "name": "balanceOf",
-  "outputs": [
-    {
-      "internalType": "uint256",
-      "name": "",
-      "type": "uint256"
-    }
-  ],
-  "stateMutability": "view",
-  "type": "function"
-}, {
-  "inputs": [],
-  "name": "totalSupply",
-  "outputs": [
-    {
-      "internalType": "uint256",
-      "name": "",
-      "type": "uint256"
-    }
-  ],
-  "stateMutability": "view",
-  "type": "function"
-}, {
-  "inputs": [],
-  "name": "token0",
-  "outputs": [
-    {
-      "internalType": "contract IERC20",
-      "name": "",
-      "type": "address"
-    }
-  ],
-  "stateMutability": "view",
-  "type": "function"
-}, {
-  "inputs": [],
-  "name": "token1",
-  "outputs": [
-    {
-      "internalType": "contract IERC20",
-      "name": "",
-      "type": "address"
-    }
-  ],
-  "stateMutability": "view",
-  "type": "function"
-}]
+export const BGT_ADDRESS = "0x656b95E550C07a9ffe548bd4085c72418Ceb1dba";
+export const ERC20_ABI = [
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string"
+      }
+    ],
+    stateMutability: "pure",
+    type: "function"
+  }
+];
+export const ABI = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address"
+      }
+    ],
+    name: "balanceOf",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256"
+      }
+    ],
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256"
+      }
+    ],
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    name: "token0",
+    outputs: [
+      {
+        internalType: "contract IERC20",
+        name: "",
+        type: "address"
+      }
+    ],
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    name: "token1",
+    outputs: [
+      {
+        internalType: "contract IERC20",
+        name: "",
+        type: "address"
+      }
+    ],
+    stateMutability: "view",
+    type: "function"
+  }
+];
 export const VAULT_ADDRESS_ABI = [
   {
-    "constant": false,
-    "inputs": [
+    constant: false,
+    inputs: [
       {
-        "name": "amount",
-        "type": "uint256"
+        name: "amount",
+        type: "uint256"
       }
     ],
-    "name": "stake",
-    "outputs": [
+    name: "stake",
+    outputs: [
       {
-        "name": "",
-        "type": "bool"
+        name: "",
+        type: "bool"
       }
     ],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
   },
   {
-    "type": "function",
-    "name": "withdraw",
-    "inputs": [
+    type: "function",
+    name: "withdraw",
+    inputs: [
       {
-        "name": "_shareAmt",
-        "type": "uint256",
-        "internalType": "uint256"
+        name: "_shareAmt",
+        type: "uint256",
+        internalType: "uint256"
       }
     ],
-    "outputs": [],
-    "stateMutability": "nonpayable"
+    outputs: [],
+    stateMutability: "nonpayable"
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "name": "game",
-        "type": "address"
+        name: "game",
+        type: "address"
       }
     ],
-    "name": "getReward",
-    "outputs": [
+    name: "getReward",
+    outputs: [
       {
-        "name": "",
-        "type": "address"
+        name: "",
+        type: "address"
       }
     ],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    stateMutability: "nonpayable",
+    type: "function"
   },
   {
-    "type": "function",
-    "name": "earned",
-    "inputs": [
+    type: "function",
+    name: "earned",
+    inputs: [
       {
-        "name": "account",
-        "type": "address",
-        "internalType": "address"
+        name: "account",
+        type: "address",
+        internalType: "address"
       }
     ],
-    "outputs": [
+    outputs: [
       {
-        "name": "",
-        "type": "uint256",
-        "internalType": "uint256"
+        name: "",
+        type: "uint256",
+        internalType: "uint256"
       }
     ],
-    "stateMutability": "view"
+    stateMutability: "view"
   }
-]
+];
 export type DataType = {
   count: number | string;
   totalSupply?: any;
-}
+};
 export function useBGT(tab?: string) {
   const isMobile = useIsMobile();
   const toast = useToast();
-  const router = useRouter()
+  const router = useRouter();
   const { handleReport } = useClickTracking();
-  const { provider, account, chainId } = useCustomAccount()
+  const { provider, account, chainId } = useCustomAccount();
   const [data, setData] = useState<DataType>({
-    count: 0,
-  })
-  const [yourVaults, setYourVaults] = useState([])
+    count: 0
+  });
+  const [yourVaults, setYourVaults] = useState<any>([]);
 
-  const [updater, setUpdater] = useState(0)
-  const { loading, dataList } = useInfraredList(updater)
+  const [updater, setUpdater] = useState(0);
+  const { loading, dataList } = useInfraredList(updater);
 
-  const [isLoading, setIsLoading] = useState(false)
-  const [sortDataIndex, setSortDataIndex] = useState("")
-  const [myVaults, setMyVaults] = useState<any>(null)
+  const [isLoading, setIsLoading] = useState(false);
+  const [sortDataIndex, setSortDataIndex] = useState("");
+  const [myVaults, setMyVaults] = useState<any>(null);
 
-  const [pageData, setPageData] = useState<any>(null)
+  const [pageData, setPageData] = useState<any>(null);
 
-  const multicallAddress = multicallAddresses[chainId];
+  const multicallAddress = multicallAddresses[DEFAULT_CHAIN_ID];
 
-  const filterList = useMemo(() => {
-    return sortDataIndex
-      ? _.cloneDeep(yourVaults).sort((prev, next) =>
+  const filterList = sortDataIndex
+    ? // @ts-ignore
+      _.cloneDeep(yourVaults).sort((prev, next) =>
         Big(next[sortDataIndex]).minus(prev[sortDataIndex]).toFixed()
       )
-      : yourVaults
-  })
+    : yourVaults;
   const queryPageData = async function () {
-    const result = await asyncFetch("https://bartio-pol-indexer.berachain.com/berachain/v1alpha1/beacon/homepage")
+    const result = await asyncFetch(
+      "https://bartio-pol-indexer.berachain.com/berachain/v1alpha1/beacon/homepage"
+    );
     if (result?.top3EmittingValidators?.validators) {
       result.top3EmittingValidators.validators.forEach((v: any) => {
         if (!v.validator?.metadata) return;
         switch (v.validator.metadata.name) {
-          case 'Infrared':
-            v.validator.metadata.bp = '1010-004-001';
-            v.validator.metadata.bpMobile = '1016-003';
+          case "Infrared":
+            v.validator.metadata.bp = "1010-004-001";
+            v.validator.metadata.bpMobile = "1016-003";
             break;
-          case 'Kodiak Finance':
-            v.validator.metadata.bp = '1010-004-002';
-            v.validator.metadata.bpMobile = '1016-004';
+          case "Kodiak Finance":
+            v.validator.metadata.bp = "1010-004-002";
+            v.validator.metadata.bpMobile = "1016-004";
             break;
-          case 'The-Honey-Jar':
-            v.validator.metadata.bp = '1010-004-003';
-            v.validator.metadata.bpMobile = '1016-005';
+          case "The-Honey-Jar":
+            v.validator.metadata.bp = "1010-004-003";
+            v.validator.metadata.bpMobile = "1016-005";
             break;
           default:
             break;
         }
       });
     }
-    setPageData(result)
-  }
+    setPageData(result);
+  };
 
   const refresh = function () {
-    setUpdater(Date.now())
-  }
+    setUpdater(Date.now());
+  };
 
   const queryData = async function () {
-    const contract = new ethers.Contract(BGT_ADDRESS, ABI, provider?.getSigner())
+    const contract = new ethers.Contract(
+      BGT_ADDRESS,
+      ABI,
+      provider?.getSigner()
+    );
     try {
-      const balanceOfResult = await contract.balanceOf(account)
-      const totalSupplyResult = await contract.totalSupply()
+      const balanceOfResult = await contract.balanceOf(account);
+      const totalSupplyResult = await contract.totalSupply();
 
-      console.log('===totalSupplyResult', totalSupplyResult)
+      console.log("===totalSupplyResult", totalSupplyResult);
       setData((prev: DataType) => {
         return {
           ...prev,
           count: ethers.utils.formatUnits(balanceOfResult),
           totalSupply: ethers.utils.formatUnits(totalSupplyResult)
-        }
-      })
+        };
+      });
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
-  const setClaiming = function (index, claiming) {
-    setYourVaults(prev => {
-      const curr = _.cloneDeep(prev)
-      curr[index].claiming = claiming
-      return curr
-    })
-  }
+  const setClaiming = function (index: any, claiming: any) {
+    setYourVaults((prev: any) => {
+      const curr: any = _.cloneDeep(prev);
+      curr[index].claiming = claiming;
+      return curr;
+    });
+  };
   const handleClaim = function (data: any, index: number) {
-
-    console.log('---data', data)
+    console.log("---data", data);
     const toastId = toast?.loading({
       title: `Claim...`
     });
 
-    setClaiming(index, true)
+    setClaiming(index, true);
 
-    const contract = new ethers.Contract(data?.vaultAddress, VAULT_ADDRESS_ABI, provider.getSigner())
+    const contract = new ethers.Contract(
+      data?.vaultAddress,
+      VAULT_ADDRESS_ABI,
+      provider.getSigner()
+    );
     contract
       .getReward(account)
       .then((tx: any) => tx.wait())
       .then((receipt: any) => {
         toast?.dismiss(toastId);
         toast?.success({
-          title: 'Claim Successful!'
+          title: "Claim Successful!"
         });
-        setClaiming(index, false)
-        refresh()
+        setClaiming(index, false);
+        refresh();
       })
       .catch((error: Error) => {
-        console.log('error: ', error);
+        console.log("error: ", error);
         toast?.dismiss(toastId);
-        setClaiming(index, false)
+        setClaiming(index, false);
         toast?.fail({
-          title: 'Claim Failed!',
-          text: error?.message?.includes('user rejected transaction')
-            ? 'User rejected transaction'
-            : (error?.message ?? '')
+          title: "Claim Failed!",
+          text: error?.message?.includes("user rejected transaction")
+            ? "User rejected transaction"
+            : error?.message ?? ""
         });
       });
-  }
+  };
 
   const handleExplore = function () {
-    router.push("/marketplace/invest?type=vaults")
-    handleReport('1010-004-004');
-  }
+    router.push("/marketplace/invest?type=vaults");
+    handleReport("1010-004-004");
+  };
 
   const handleValidator = (data: any) => {
     if (isMobile) {
@@ -293,71 +308,85 @@ export function useBGT(tab?: string) {
 
   const queryYourVaults = async () => {
     try {
-      setIsLoading(true)
-      const response = await asyncFetch("https://bartio-pol-indexer.berachain.com/berachain/v1alpha1/beacon/user/" + account + "/vaults")
-      const vaults = response?.userVaults
-      const depositedAmountCalls = []
-      const bgtRewardsCalls = []
-      vaults.forEach(valut => {
+      setIsLoading(true);
+      const response = await asyncFetch(
+        "https://bartio-pol-indexer.berachain.com/berachain/v1alpha1/beacon/user/" +
+          account +
+          "/vaults"
+      );
+      const vaults = response?.userVaults;
+      const depositedAmountCalls: any = [];
+      const bgtRewardsCalls: any = [];
+      vaults.forEach((valut: any) => {
         depositedAmountCalls.push({
           address: valut?.vaultAddress,
-          name: 'balanceOf',
+          name: "balanceOf",
           params: [account]
-        })
+        });
         bgtRewardsCalls.push({
           address: valut?.vaultAddress,
-          name: 'earned',
+          name: "earned",
           params: [account]
-        })
-      })
+        });
+      });
       const depositedAmountResult = await multicall({
         abi: ABI,
         options: {},
         calls: depositedAmountCalls,
         multicallAddress,
         provider
-      })
+      });
       const bgtRewardsResult = await multicall({
         abi: VAULT_ADDRESS_ABI,
         options: {},
         calls: bgtRewardsCalls,
         multicallAddress,
         provider
-      })
+      });
 
-      const _yourVaults = []
+      const _yourVaults = [];
       for (let i = 0; i < vaults.length; i++) {
         if (depositedAmountResult[i]) {
           _yourVaults.push({
             ...vaults[i],
-            depositedAmount: ethers.utils.formatUnits(depositedAmountResult?.[i][0] ?? 0),
-            earned: bgtRewardsResult[i] ? ethers.utils.formatUnits(bgtRewardsResult?.[i][0] ?? 0) : 0,
-            earnedShown: numberFormatter(bgtRewardsResult[i] ? ethers.utils.formatUnits(bgtRewardsResult?.[i][0] ?? 0) : 0, 6, true),
+            depositedAmount: ethers.utils.formatUnits(
+              depositedAmountResult?.[i][0] ?? 0
+            ),
+            earned: bgtRewardsResult[i]
+              ? ethers.utils.formatUnits(bgtRewardsResult?.[i][0] ?? 0)
+              : 0,
+            earnedShown: numberFormatter(
+              bgtRewardsResult[i]
+                ? ethers.utils.formatUnits(bgtRewardsResult?.[i][0] ?? 0)
+                : 0,
+              6,
+              true
+            ),
             claim: handleClaim
-          })
+          });
         }
       }
-      setIsLoading(false)
-      setYourVaults(_yourVaults)
+      setIsLoading(false);
+      setYourVaults(_yourVaults);
     } catch (error) {
-      console.error(error)
-      setIsLoading(false)
+      console.error(error);
+      setIsLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
-    queryPageData()
-  }, [])
+    queryPageData();
+  }, []);
 
   useEffect(() => {
     if (tab === "your" && account) {
-      queryYourVaults()
+      queryYourVaults();
     }
-  }, [tab, account, updater])
+  }, [tab, account, updater]);
 
   useEffect(() => {
-    provider && account && queryData()
-  }, [provider, account])
+    provider && account && queryData();
+  }, [provider, account]);
 
   return {
     data,
@@ -372,9 +401,8 @@ export function useBGT(tab?: string) {
     queryPageData,
     handleClaim,
     handleExplore,
-    handleValidator,
-
-  }
+    handleValidator
+  };
 }
 
 interface Props {
