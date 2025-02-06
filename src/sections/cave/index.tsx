43b148d2a12aa1bcc5c77ca122af63aeb571460e
jimmygu <jimmyguu2024@gmail.com>
2025-02-06 15:35:48 +0800

    feat: modify cave Transfer Boost Items styles

--- src/sections/cave/index.tsx
+++ src/sections/cave/index.tsx
@@ -702,10 +702,12 @@ export default function Cave() {
                 setTransferItemsVisible(true)
                 setTransferSelectedItems(transferItems)
             }}
-            className="pb-[5px] absolute right-[18px] bottom-[78px] w-[129px] rounded-[10px] border border-[#709D27] bg-[#7DB425] backdrop-blur-[5px]">
-            <div className="active:translate-y-[5px] relative cursor-pointer h-[40px] rounded-[10px] border border-[#709D27] bg-[#C7FF6E] backdrop-blur-[5px]">
-                <div className="p-[5px_0_0_12px] w-[92px] text-[#F7F9EA] font-CherryBomb text-[14px] text-stroke-1-4b371f leading-[100%]">Transfer Boost Items</div>
-                <div className="absolute top-[6px] right-[7px] w-[26px]">
+            className="pb-[5px] absolute right-[20px] bottom-[80px] w-[200px] rounded-[10px] border border-[#709D27] bg-[#7DB425] backdrop-blur-[5px]">
+            <div className="w-full flex justify-center items-center gap-[20px] active:translate-y-[5px] relative cursor-pointer h-[60px] rounded-[10px] border border-[#709D27] bg-[#C7FF6E] backdrop-blur-[5px]">
+                <div className="pl-[12px] w-0 flex-1 text-[#F7F9EA] font-CherryBomb text-[18px] text-stroke-1-4b371f leading-[100%]">
+                    Transfer Boost Items
+                </div>
+                <div className="mr-[12px] w-[40px] shrink-0">
                     <img src="/images/cave/icon-bear.png" alt="icon-bear" />
                 </div>
             </div>
