4856a213a79eb8f9bfad9eea294377d92eb4dca1
Emily <emily@ref.finance>
2025-02-07 20:47:04 +0800

    add fixed buy.

--- src/sections/ramen/hooks/use-fixed-buy.ts
+++ src/sections/ramen/hooks/use-fixed-buy.ts
@@ -1,50 +1,39 @@
 import { useState } from "react";
 import useToast from "@/hooks/use-toast";
-import { TOKENS } from "../config";
 import { asyncFetch } from "@/utils/http";
 import useCustomAccount from "@/hooks/use-account";
 import { useSignMessage } from "wagmi";
 
-export default function useBuy({ detail, amount }: any) {
+export default function useFixedBuy({ detail, amount }: any) {
   const [buying, setBuying] = useState(false);
   const toast = useToast();
   const { account } = useCustomAccount();
   const { data: signature, signMessageAsync } = useSignMessage();
 
-  const fixedBuy = async () => {
-    await signMessageAsync({
-      message: `You are entering ${amount} entries into the Gacha lottery for this token launch.`
-    });
-    const res = await asyncFetch(
-      `/api.ramen/v1/project/${detail.slug}/lottery/buy`,
-      {
-        method: "POST",
-        body: JSON.stringify({
-          amount,
-          address: account,
-          signature
-        })
-      }
-    );
-    return res;
-  };
-
-  const buy = async () => {};
-
   const onBuy = async () => {
     let toastId = toast.loading({ title: "Confirming..." });
     setBuying(true);
     try {
-      const token = TOKENS[detail.slug];
-      if (token.isFixed) {
-        const res = await fixedBuy();
-        if (res.status !== "OK") {
-          throw Error("");
+      await signMessageAsync({
+        message: `You are entering ${amount} entries into the Gacha lottery for this token launch.`
+      });
+      const res = await asyncFetch(
+        `/api.ramen/v1/project/${detail.slug}/lottery/buy`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            amount,
+            address: account,
+            signature
+          })
         }
-        toast.dismiss(toastId);
-        toast.success("Buy successfully");
-        return;
+      );
+      if (res.status !== "OK") {
+        throw Error("");
       }
+      toast.dismiss(toastId);
+      toast.success("Buy successfully");
+      return;
     } catch (err: any) {
       toast.dismiss(toastId);
       toast.fail({
