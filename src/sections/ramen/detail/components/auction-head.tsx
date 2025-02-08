66fb4ea96bf0f91324a6559e07c3d8d3e8b01729
jimmygu <jimmyguu2024@gmail.com>
2025-02-08 18:50:22 +0800

    feat: ramen details styles on mobile

--- src/sections/ramen/detail/components/auction-head.tsx
+++ src/sections/ramen/detail/components/auction-head.tsx
@@ -5,7 +5,7 @@ const AuctionHead = (props: any) => {
   const { className, detail, isLaunched, countdown } = props;
 
   return (
-    <div className={clsx('mb-[37px] flex items-center gap-[10px] font-Montserrat font-[500] text-black text-[14px] leading-[90%]', className)}>
+    <div className={clsx('mb-[37px] whitespace-nowrap flex items-center gap-[10px] font-Montserrat font-[500] text-black text-[14px] leading-[90%]', className)}>
       <div className="text-[18px] font-[600]">
         {numberFormatter(detail?.bidSubmitted, 2, true)}
       </div>
@@ -15,8 +15,13 @@ const AuctionHead = (props: any) => {
           isLaunched ? 'Auction Ended' : (
             <>
               <div className="">Closed in</div>
-              <div className="font-[700]">
-                {countdown?.endSplit?.[0]}d {countdown?.endSplit?.[1]}h {countdown?.endSplit?.[2]}m {countdown?.endSplit?.[3]}s
+              <div className="font-[700] flex items-center gap-[5px]">
+                <div>
+                  {countdown?.endSplit?.[0]}d {countdown?.endSplit?.[1]}h {countdown?.endSplit?.[2]}m
+                </div>
+                <div className="md:hidden">
+                  {countdown?.endSplit?.[3]}s
+                </div>
               </div>
             </>
           )
