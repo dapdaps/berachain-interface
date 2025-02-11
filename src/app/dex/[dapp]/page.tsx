e4a537c0154ec414b5acde386650541bcea05039
Emily <emily@ref.finance>
2025-02-11 12:07:07 +0800

    add dex tokens.

--- src/app/dex/[dapp]/page.tsx
+++ src/app/dex/[dapp]/page.tsx
@@ -1,12 +1,13 @@
-'use client';
+"use client";
 
-import SwapView from '@/sections/swap';
-import { useParams } from 'next/navigation';
-import dapps from '@/configs/swap';
-import { DEFAULT_SWAP_DAPP } from '@/configs';
-import useClickTracking from '@/hooks/use-click-tracking';
-import { useEffect } from 'react';
-import useIsMobile from '@/hooks/use-isMobile';
+import SwapView from "@/sections/swap";
+import { useParams } from "next/navigation";
+import dapps from "@/configs/swap";
+import { DEFAULT_SWAP_DAPP, DEFAULT_CHAIN_ID } from "@/configs";
+import useClickTracking from "@/hooks/use-click-tracking";
+import { useEffect, useMemo } from "react";
+import useIsMobile from "@/hooks/use-isMobile";
+import useDexTokens from "@/hooks/use-dex-tokens";
 
 export default function SwapPage() {
   const urlParams = useParams();
@@ -14,22 +15,32 @@ export default function SwapPage() {
   const isMobile = useIsMobile();
 
   const dapp = dapps[urlParams.dapp as string] || dapps[DEFAULT_SWAP_DAPP];
-
+  const tokens = useDexTokens(dapp);
   useEffect(() => {
     switch (urlParams.dapp) {
-      case 'kodiak':
-        handleReport(isMobile ? '1017-004' : '1012-004');
+      case "kodiak":
+        handleReport(isMobile ? "1017-004" : "1012-004");
         break;
-      case 'bex':
-        handleReport(isMobile ? '1017-005' : '1012-005');
+      case "bex":
+        handleReport(isMobile ? "1017-005" : "1012-005");
         break;
-      case 'ooga-booga':
-        handleReport(isMobile ? '1017-006' : '1012-006');
+      case "ooga-booga":
+        handleReport(isMobile ? "1017-006" : "1012-006");
         break;
       default:
         break;
     }
   }, [isMobile]);
 
-  return <SwapView dapp={dapp} />;
+  const dappConfig = useMemo(
+    () => ({
+      ...dapp,
+      tokens: {
+        [DEFAULT_CHAIN_ID]: tokens
+      }
+    }),
+    [dapp, tokens]
+  );
+
+  return <SwapView dapp={dappConfig} />;
 }
