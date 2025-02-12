e5357f01269ff27ff418916990cf62d40b85c51b
Emily <emily@ref.finance>
2025-02-12 17:20:11 +0800

    fix token symbol.

--- src/utils/token/symbol.ts
+++ src/utils/token/symbol.ts
@@ -1,6 +1,7 @@
 export const getReportTokenSymbol = (token: any) => {
   if (
-    token.address.toLowerCase() === "0x0d9ac083dD2760943F773E70EbfFe621e950871c"
+    token.address.toLowerCase() ===
+    "0x0d9ac083dD2760943F773E70EbfFe621e950871c".toLowerCase()
   )
     return "BTCLUB";
   return token.symbol;
