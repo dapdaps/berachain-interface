d008e17d90e8488428d1b73dfaa473e54609127a
Emily <emily@ref.finance>
2025-02-12 16:19:28 +0800

    fix smybol uncorrect.

--- src/utils/token/symbol.ts
+++ src/utils/token/symbol.ts
@@ -3,6 +3,5 @@ export const getReportTokenSymbol = (token: any) => {
     token.address.toLowerCase() === "0x0d9ac083dD2760943F773E70EbfFe621e950871c"
   )
     return "BTCLUB";
-  return;
-  token.symbol;
+  return token.symbol;
 };
