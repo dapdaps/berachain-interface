7140dcfeb2c94e24f444f8e72ee0ab51b56284db
Emily <emily@ref.finance>
2025-02-19 10:10:47 +0800

    fix count down.

--- src/components/airdrop/claim-count-down.tsx
+++ src/components/airdrop/claim-count-down.tsx
@@ -1,13 +1,13 @@
-import { useCountDown } from "@/sections/kingdomly/hooks/use-count-down";
+import useCountDown, { getTimePeriods, toTwo } from "@/hooks/use-count-down";
 
-export default function ClaimCountDown({ time }: any) {
-  const countdown = useCountDown({
-    targetTimestamp: time,
-    format: "DDd : HHh : mmm : sss"
-  });
+export default function ClaimCountDown({ time = 0 }: any) {
+  const { secondsRemaining } = useCountDown(time / 1000);
+  const timeLeft = getTimePeriods(secondsRemaining);
+
   return (
     <div className="text-[26px] text-center font-bold font-CherryBomb">
-      {countdown}
+      {toTwo(timeLeft.days)}d : {toTwo(timeLeft.hours)}h :{" "}
+      {toTwo(timeLeft.minutes)}m : {toTwo(timeLeft.seconds)}s
     </div>
   );
 }
