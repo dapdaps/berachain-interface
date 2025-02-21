40fba6dfcc3de7b237426f21e1f3037e17a4b5c8
GG-Balder <154797555+GG-Balder@users.noreply.github.com>
2025-02-21 15:50:46 +0800

    fix: mobile open entry

--- src/sections/dapps/index.tsx
+++ src/sections/dapps/index.tsx
@@ -207,7 +207,6 @@ const ALL_LIST = [
       {
         ..._dApps["infrared"],
         attachedIcon: "",
-        disabled: true
       }
     ]
   },
@@ -315,7 +314,6 @@ const ALL_LIST = [
         attachedIcon: (
           <TrunkLarge className="absolute left-[44%] bottom-[-28%]" />
         ),
-        disabled: true
       },
       {
         ..._dApps["bedrock"],
