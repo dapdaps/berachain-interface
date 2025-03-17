e3fc949616c6f1245f08261c96dbfb859ae0450a
GG-Balder <154797555+GG-Balder@users.noreply.github.com>
2025-03-17 14:42:06 +0800

    fix: fix infrared position hover

--- src/sections/bgt/components/ibgt-amount.tsx
+++ src/sections/bgt/components/ibgt-amount.tsx
@@ -12,17 +12,17 @@ export default memo(function IbgtAmount({
   className: string,
   symbol: string
 }) {
-  return symbol === "iBGT" ? (
+  return (
     <Popover
       placement={PopoverPlacement.BottomLeft}
       content={
-        <div className='relative pt-[19px] px-[19px] pb-[23px] w-[200px] h-[110px] rounded-[20px] border border-black bg-[#FFFDEB] shadow-[10px_10px_0px_0px_rgba(0,0,0,0.25)]'>
+        <div className='relative pt-[19px] px-[19px] pb-[23px] min-w-[200px] h-[110px] rounded-[20px] border border-black bg-[#FFFDEB] shadow-[10px_10px_0px_0px_rgba(0,0,0,0.25)]'>
           <div className='flex flex-col gap-[13px]'>
             <div className='text-[#3D405A] font-Montserrat text-[14px] font-medium'>
               Amount
             </div>
-            <div className='text-black font-Montserrat text-[20px] font-semibold leading-[90%]'>
-              {numberFormatter(amount, 3, true)}{' '}iBGT
+            <div className='text-black font-Montserrat text-[16px] font-semibold leading-[90%]'>
+              {numberFormatter(amount, 3, true)}{' '}{symbol}
             </div>
           </div>
         </div>
@@ -34,11 +34,5 @@ export default memo(function IbgtAmount({
         })}
       </div>
     </Popover>
-  ) : (
-    <div className={className}>
-      {numberFormatter(usdAmount, 2, true, {
-        prefix: "$"
-      })}
-    </div>
   )
 })
