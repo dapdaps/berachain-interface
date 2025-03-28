878e90e715994a7f0a82739fb0c7163efd3e1145
jimmygu <jimmyguu2024@gmail.com>
2025-03-28 17:22:07 +0800

    feat: Update signpost component with dynamic styles and text
    Added a toggle for switching between two background designs and updated content dynamically based on the `OPEN_BTC_LST` flag. Enhanced descriptions and button labels for better user engagement, and included a new SVG for alternative styling.

--- src/sections/home-earth/components/signpost-bubble.tsx
+++ src/sections/home-earth/components/signpost-bubble.tsx
@@ -4,7 +4,7 @@ const SignpostBubble = (props: any) => {
   const { children, className, leftArrowY } = props;
 
   return (
-    <div className={clsx("relative w-[304px] h-[179px] flex-shrink-0 bg-[#FFFDEB] border border-black shadow-[10px_10px_0px_rgba(0,0,0,0.25)] rounded-[20px] text-black font-CherryBomb text-[16px] font-normal leading-[120%] p-[19px_18px_22px_18px]", className)}>
+    <div className={clsx("relative w-[304px] h-[186px] flex-shrink-0 bg-[#FFFDEB] border border-black shadow-[10px_10px_0px_rgba(0,0,0,0.25)] rounded-[20px] text-black font-CherryBomb text-[16px] font-normal leading-[120%] p-[15px_18px_20px_18px]", className)}>
       <img
         src="/images/home-earth/signpost-bubble-arrow.svg"
         alt=""
--- src/sections/home-earth/components/signpost.tsx
+++ src/sections/home-earth/components/signpost.tsx
@@ -4,40 +4,54 @@ import SignpostBubble from '@/sections/home-earth/components/signpost-bubble';
 import Popover, { PopoverPlacement, PopoverTrigger } from '@/components/popover';
 import useIsMobile from '@/hooks/use-isMobile';
 
+const OPEN_BTC_LST = false;
+
 const Signpost = (props: any) => {
   const { className } = props;
   const router = useRouter();
   const isMobile = useIsMobile();
 
   return (
-    <div className={clsx('absolute left-0 bottom-0 z-[5] w-[178px] h-[277px] overflow-hidden flex justify-center bg-[url("/images/home-earth/signpost.svg")] bg-no-repeat bg-[center_30px] bg-contain', className)}>
+    <div
+      className={clsx(
+        "absolute left-0 bottom-0 z-[5] overflow-hidden flex justify-center bg-no-repeat bg-contain",
+        OPEN_BTC_LST ? "bg-[url('/images/home-earth/signpost.svg')] w-[178px] h-[277px] bg-[center_30px]" : "bg-[url('/images/home-earth/signpost-2.svg')] w-[146px] h-[191px] bg-[center_10px]",
+        className
+      )}
+    >
       <img
         src="/images/home-earth/signpost-bintent.png"
         alt=""
         onClick={() => router.push('/bintent')}
-        className="w-[94px] h-[27px] absolute top-[58px] left-[36px] cursor-pointer object-center object-contain"
+        className={clsx(
+          "w-[94px] h-[27px] absolute left-[36px] cursor-pointer object-center object-contain",
+          OPEN_BTC_LST ? "top-[58px]" : "top-[32px]"
+        )}
       />
       <Popover
         placement={PopoverPlacement.Right}
         trigger={isMobile ? PopoverTrigger.Click : PopoverTrigger.Hover}
-        triggerContainerClassName="absolute left-[20px] top-[112px]"
+        triggerContainerClassName={clsx(
+          "absolute left-[20px]",
+          OPEN_BTC_LST ? "top-[112px]" : "top-[82px]"
+        )}
         contentClassName={isMobile ? "!z-[53]" : ""}
         offset={50}
         // closeDelayDuration={0}
         content={(
           <SignpostBubble className={clsx("", isMobile && "scale-[0.8322]")}>
             <div className="text-[#FF7040] font-CherryBomb text-[26px] font-normal not-italic leading-[90%] text-stroke-1">
-              APR up to 300%!
+              APR up to 700%!
             </div>
-            <div className="mt-[11px] text-[#000] font-CherryBomb text-[16px] font-normal not-italic leading-[120%]">
-              Join vaults and earn money easily in BeraTown, be a happy gummy bear
+            <div className="mt-[11px] text-[#000] font-CherryBomb text-[16px] font-normal not-italic leading-[110%]">
+              Beratown’s Vaults is easiest way to discover, navigate, and access da best Berachain & POL yield!
             </div>
             <button
               type="button"
               className="mt-[16px] h-[50px] flex-shrink-0 w-full rounded-[10px] border border-[#000] bg-[#FFDC50] shadow-[6px_6px_0px_rgba(0,0,0,0.25)] text-[#000] text-center font-Montserrat text-[16px] font-bold leading-normal"
               onClick={() => router.push('/vaults')}
             >
-              Join Now
+              Get me in
             </button>
           </SignpostBubble>
         )}
@@ -52,12 +66,16 @@ const Signpost = (props: any) => {
           className="w-[110px] h-[47px] cursor-pointer object-center object-contain"
         />
       </Popover>
-      <img
-        src="/images/home-earth/signpost-btc-lst.png"
-        alt=""
-        onClick={() => {}}
-        className="w-[77px] h-[17px] absolute left-[66px] top-[196px] cursor-not-allowed opacity-50 object-center object-contain"
-      />
+      {
+        OPEN_BTC_LST && (
+          <img
+            src="/images/home-earth/signpost-btc-lst.png"
+            alt=""
+            onClick={() => {}}
+            className="w-[77px] h-[17px] absolute left-[66px] top-[196px] cursor-not-allowed opacity-50 object-center object-contain"
+          />
+        )
+      }
     </div>
   );
 };
