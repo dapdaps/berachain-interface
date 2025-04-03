568da113aaed353193102537d5a6b223873e708e
GG-Balder <154797555+GG-Balder@users.noreply.github.com>
2025-04-03 16:48:41 +0800

    fix: fix some bugs

--- src/sections/bintent-trading-challenge/mobile.tsx
+++ src/sections/bintent-trading-challenge/mobile.tsx
@@ -5,12 +5,10 @@ import AirshipSvg from "@public/images/campaign/mobile/airship.svg";
 
 export default memo(function Mobile() {
   return (
-    <div className="absolute left-0 top-0 right-0 bottom-0">
-      <AirshipSvg className="absolute left-0 top-[50px] z-50" />
-      <div className="px-[8px] absolute left-0 top-0 right-0 bottom-0 z-30 overflow-auto">
-        <TaskBoard />
-        <Leaderboard />
-      </div>
+    <div className="pt-[50px] px-[8px] absolute left-0 top-0 right-0 bottom-0 z-30 overflow-auto">
+      <AirshipSvg />
+      <TaskBoard />
+      <Leaderboard />
     </div>
   )
 })
