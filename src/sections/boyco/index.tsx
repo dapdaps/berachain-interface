def44494cc746a0d37826581663652db03b89328
Emily <emily@ref.finance>
2025-05-04 23:52:36 +0800

    add provider to vaults.

--- src/sections/boyco/index.tsx
+++ src/sections/boyco/index.tsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import HomeEntry from "./components/home-entry";
 import BoycoModal from "./components/modal";
-import { RoycoProvider } from "royco";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import BoycoProvider from "./boyco-provider";
 
 export const RPC_API_KEYS: {
   [chain_id: number]: string;
@@ -12,42 +11,18 @@ export const RPC_API_KEYS: {
 
 export default function Boyco() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            retry: 3,
-            refetchOnWindowFocus: false,
-            refetchIntervalInBackground: true,
-            refetchOnReconnect: false,
-            staleTime: 1000 * 60 * 60, // 1 hour
-            gcTime: 1000 * 60 * 60 * 24 // 1 day
-          }
-        }
-      })
-  );
 
   return (
     <>
       <HomeEntry onOpenModal={() => setIsModalOpen(true)} />
       {isModalOpen && (
-        <QueryClientProvider client={queryClient}>
-          <RoycoProvider
-            originUrl={"https://istbjtfzjcnstpzunkje-all.supabase.co"}
-            originKey={
-              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzdGJqdGZ6amNuc3RwenVua2plIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgxODc3NDUsImV4cCI6MjA1Mzc2Mzc0NX0.p74w0fxcivBLkn_P82XlRG8upTCaKQDP69YsV7Ap5t0"
-            }
-            originId={"57dc9dc3-45e7-4d0c-bc97-4f33ec31c690"}
-            rpcApiKeys={RPC_API_KEYS}
-          >
-            {isModalOpen && (
-              <>
-                <BoycoModal onClose={() => setIsModalOpen(false)} />
-              </>
-            )}
-          </RoycoProvider>
-        </QueryClientProvider>
+        <BoycoProvider>
+          {isModalOpen && (
+            <>
+              <BoycoModal onClose={() => setIsModalOpen(false)} />
+            </>
+          )}
+        </BoycoProvider>
       )}
     </>
   );
--- src/sections/vaults/v2/index.tsx
+++ src/sections/vaults/v2/index.tsx
@@ -15,8 +15,9 @@ import Search from "@/sections/vaults/v2/components/filter/search";
 import Feedback from "@/sections/vaults/v2/components/feedback/feedback";
 import SubmitVault from "@/sections/vaults/v2/components/feedback/submit-vault";
 import { useRef } from "react";
-import Aggregating from '@/sections/vaults/v2/components/aggregating';
-import RewardTopCard from '@/sections/vaults/v2/components/reward-top-card';
+import Aggregating from "@/sections/vaults/v2/components/aggregating";
+import RewardTopCard from "@/sections/vaults/v2/components/reward-top-card";
+import BoycoProvider from "@/sections/boyco/boyco-provider";
 
 const VaultsV2 = (props: any) => {
   const {} = props;
@@ -26,60 +27,62 @@ const VaultsV2 = (props: any) => {
   const list = useList();
 
   return (
-    <VaultsV2ContextProvider value={{ ...vaultsV2, ...list, containerRef }}>
-      <div
-        ref={containerRef}
-        className="relative w-full min-w-[1257px] min-h-[750px] pb-[56px]"
-      >
-        <PageBack className="absolute left-[36px] top-[32px] text-white" />
-        <div className="w-[1257px] mx-auto h-[140px] pt-[20px] flex items-center justify-end relative">
-          <img
-            src="/images/vaults/v2/title.png"
-            alt=""
-            className="w-[254px] h-[120px] shrink-0 pointer-events-none object-center object-contain absolute left-1/2 -translate-x-1/2"
-          />
-          <SubmitVault className="shrink-0" />
-        </div>
-        <Aggregating />
-        <div className="w-[1257px] mx-auto pt-[42px]">
-          <div className="w-full grid grid-cols-2 gap-[20px]">
-            <RewardTopCard type={1} />
-            <RewardTopCard type={0} />
+    <BoycoProvider>
+      <VaultsV2ContextProvider value={{ ...vaultsV2, ...list, containerRef }}>
+        <div
+          ref={containerRef}
+          className="relative w-full min-w-[1257px] min-h-[750px] pb-[56px]"
+        >
+          <PageBack className="absolute left-[36px] top-[32px] text-white" />
+          <div className="w-[1257px] mx-auto h-[140px] pt-[20px] flex items-center justify-end relative">
+            <img
+              src="/images/vaults/v2/title.png"
+              alt=""
+              className="w-[254px] h-[120px] shrink-0 pointer-events-none object-center object-contain absolute left-1/2 -translate-x-1/2"
+            />
+            <SubmitVault className="shrink-0" />
           </div>
-          <div className="min-h-[320px] pt-[25px] flex justify-between items-start gap-[13px] w-full text-[14px] text-black leading-[90%] font-[500] font-Montserrat">
-            <div className="w-[337px] shrink-0">
-              <Card className="w-full !rounded-[18px] !px-0 flex flex-col">
-                <div className="w-full flex justify-between items-center gap-[15px] pb-[24px] pl-[17px] pr-[20px] border-b border-[rgba(0,0,0,0.2)]">
-                  <div className="flex items-center gap-[7px] shrink-0">
-                    <div className="text-[20px] font-[700]">Vaults</div>
-                    <div className="shrink-0 text-[12px] w-[24px] h-[24px] flex justify-center items-center rounded-[5px] border border-[rgba(0,0,0,0.2)]">
-                      {list.listLoading ? (
-                        <Loading size={12} />
-                      ) : (
-                        list.listData.length
-                      )}
-                    </div>
-                  </div>
-                  <Search className="flex-1 w-0" />
-                </div>
-                <Filter />
-              </Card>
-              <div className="w-full flex justify-center pt-[20px]">
-                <Feedback className="opacity-70" />
-              </div>
+          <Aggregating />
+          <div className="w-[1257px] mx-auto pt-[42px]">
+            <div className="w-full grid grid-cols-2 gap-[20px]">
+              <RewardTopCard type={1} />
+              <RewardTopCard type={0} />
             </div>
-            <Card className="w-0 flex-1 !rounded-[18px] !px-[18px] flex flex-col gap-[20px]">
-              <Dashboard className="shrink-0" />
-              <VaultsTable className="flex-1 h-0" />
-            </Card>
+            <div className="min-h-[320px] pt-[25px] flex justify-between items-start gap-[13px] w-full text-[14px] text-black leading-[90%] font-[500] font-Montserrat">
+              <div className="w-[337px] shrink-0">
+                <Card className="w-full !rounded-[18px] !px-0 flex flex-col">
+                  <div className="w-full flex justify-between items-center gap-[15px] pb-[24px] pl-[17px] pr-[20px] border-b border-[rgba(0,0,0,0.2)]">
+                    <div className="flex items-center gap-[7px] shrink-0">
+                      <div className="text-[20px] font-[700]">Vaults</div>
+                      <div className="shrink-0 text-[12px] w-[24px] h-[24px] flex justify-center items-center rounded-[5px] border border-[rgba(0,0,0,0.2)]">
+                        {list.listLoading ? (
+                          <Loading size={12} />
+                        ) : (
+                          list.listData.length
+                        )}
+                      </div>
+                    </div>
+                    <Search className="flex-1 w-0" />
+                  </div>
+                  <Filter />
+                </Card>
+                <div className="w-full flex justify-center pt-[20px]">
+                  <Feedback className="opacity-70" />
+                </div>
+              </div>
+              <Card className="w-0 flex-1 !rounded-[18px] !px-[18px] flex flex-col gap-[20px]">
+                <Dashboard className="shrink-0" />
+                <VaultsTable className="flex-1 h-0" />
+              </Card>
+            </div>
           </div>
+          <ActionModal />
+          <ClaimModal />
+          <ClaimSuccessModal />
+          <StrategyModal pool={list.listDataHotStrategy} />
         </div>
-        <ActionModal />
-        <ClaimModal />
-        <ClaimSuccessModal />
-        <StrategyModal pool={list.listDataHotStrategy} />
-      </div>
-    </VaultsV2ContextProvider>
+      </VaultsV2ContextProvider>
+    </BoycoProvider>
   );
 };
 
