dde41574d9bccc042249dca19df772999e49e8c2
Emily <emily@ref.finance>
2025-05-05 09:33:15 +0800

    fix vaults

--- src/sections/boyco/components/vaults/list.tsx
+++ src/sections/boyco/components/vaults/list.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/navigation";
 export default function List({ vaults, loading }: any) {
   const router = useRouter();
   return (
-    <div className="flex flex-col gap-[10px] mt-[14px] max-h-[calc(100%-206px)] overflow-y-auto">
+    <div className="mt-[14px] ">
       {loading ? (
         <div className="flex justify-center items-center h-[400px]">
           <Loading size={20} />
@@ -20,45 +20,47 @@ export default function List({ vaults, loading }: any) {
             </div>
           ) : (
             <>
-              {vaults?.map((item: any, index: number) => (
-                <div
-                  key={index}
-                  className="flex items-center justify-between h-[36px] w-full border border-[#392C1D] pr-[14px]"
-                >
-                  <div className="grow flex items-center">
-                    <div
-                      className="relative h-full flex justify-center"
-                      style={{
-                        width: 50 + (item.tokens.length - 1) * 16
-                      }}
-                    >
-                      <LabelIcon
-                        width={50 + (item.tokens.length - 1) * 16}
-                        className="absolute top-[-5px] left-[0px] z-[1]"
-                      />
-                      <div className="flex relative z-[2]">
-                        {item.tokens.map((token: any, index: number) => (
-                          <img
-                            key={index}
-                            src={token.logo || token.icon}
-                            className={clsx(
-                              "w-[26px] h-[26px] rounded-full",
-                              index !== 0 && "ml-[-14px]"
-                            )}
-                          />
-                        ))}
+              <div className="max-h-[calc(100%-206px)] overflow-y-auto flex flex-col gap-[10px]">
+                {vaults?.map((item: any, index: number) => (
+                  <div
+                    key={index}
+                    className="flex items-center justify-between h-[36px] w-full border border-[#392C1D] pr-[14px]"
+                  >
+                    <div className="grow flex items-center">
+                      <div
+                        className="relative h-full flex justify-center"
+                        style={{
+                          width: 50 + (item.tokens.length - 1) * 16
+                        }}
+                      >
+                        <LabelIcon
+                          width={50 + (item.tokens.length - 1) * 16}
+                          className="absolute top-[-5px] left-[0px] z-[1]"
+                        />
+                        <div className="flex relative z-[2]">
+                          {item.tokens.map((token: any, index: number) => (
+                            <img
+                              key={index}
+                              src={token.logo || token.icon}
+                              className={clsx(
+                                "w-[26px] h-[26px] rounded-full",
+                                index !== 0 && "ml-[-14px]"
+                              )}
+                            />
+                          ))}
+                        </div>
+                      </div>
+                      <div className="text-[14px] text-[#392C1D] leading-[90%]">
+                        Join <span className="font-bold">{item.name}</span>{" "}
+                        vault , APY up to{" "}
+                        <span className="font-bold">
+                          {item.totalApy[1].toFixed(2)}%
+                        </span>
                       </div>
                     </div>
-                    <div className="text-[14px] text-[#392C1D] leading-[90%]">
-                      Join <span className="font-bold">{item.name}</span> vault
-                      , APY up to{" "}
-                      <span className="font-bold">
-                        {item.totalApy[1].toFixed(2)}%
-                      </span>
-                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
               <button
                 onClick={() => {
                   router.push("/vaults?from=boyco");
