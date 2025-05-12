80e7b1467fa769a9127ff6a204fccdaff216d36e
jimmygu <jimmyguu2024@gmail.com>
2025-05-12 17:32:35 +0800

    feat: Add USD value display for rewards across components
    Introduced logic to calculate and display rewards in USD using price data from usePriceStore. Enhanced UI components and styling adjustments to support the new feature without altering existing layouts.

--- src/sections/vaults/v2/components/claim/index.tsx
+++ src/sections/vaults/v2/components/claim/index.tsx
@@ -5,12 +5,15 @@ import useClaim from "../../hooks/use-claim";
 import { useVaultsV2Context } from "@/sections/vaults/v2/context";
 import { numberFormatter } from "@/utils/number-formatter";
 import { useMemo } from "react";
+import { usePriceStore } from '@/stores/usePriceStore';
+import Big from 'big.js';
 
 const Claim = (props: any) => {
   const { className } = props;
 
   const { loading, onClaim } = useClaim();
   const { currentReward, currentProtocol } = useVaultsV2Context();
+  const prices = usePriceStore(store => store.beraTownPrice);
 
   const rewards = useMemo(
     () => (currentReward.splice ? currentReward : [currentReward]),
@@ -18,6 +21,16 @@ const Claim = (props: any) => {
   );
 
   if (currentProtocol.protocol === "D2 Finance") return;
+
+  const getRewardUsd = (reward: any) => {
+    if (!reward) return null;
+    let price = prices[reward.symbol];
+    if (!price) {
+      price = prices[reward.address];
+    }
+    if (!price) return null;
+    return numberFormatter(Big(price).times(reward.amount || 0), 2, true, { prefix: "$", isShort: true, isShortUppercase: true, isZeroPrecision: true });
+  };
 
   return (
     <div
@@ -27,30 +40,33 @@ const Claim = (props: any) => {
       )}
     >
       <div className="">Claim Rewards</div>
-      {rewards.map((reward: any) => (
-        <div
-          className="mt-[30px] flex flex-col items-center"
-          key={reward.address}
-        >
-          <LazyImage
-            src={reward?.icon}
-            width={36}
-            height={36}
-            containerClassName="shrink-0 rounded-full overflow-hidden"
-            fallbackSrc="/assets/tokens/default_icon.png"
-          />
-          <div className="mt-[15px] text-[20px] font-[600]">
-            {numberFormatter(reward?.amount, 6, true, {
-              isShort: true,
-              isShortUppercase: true
-            })}{" "}
-            {reward?.symbol}
+      {rewards.map((reward: any) => {
+        const rewardUsd = getRewardUsd(reward);
+        return (
+          <div
+            className="mt-[30px] flex flex-col items-center"
+            key={reward.address}
+          >
+            <LazyImage
+              src={reward?.icon}
+              width={36}
+              height={36}
+              containerClassName="shrink-0 rounded-full overflow-hidden"
+              fallbackSrc="/assets/tokens/default_icon.png"
+            />
+            <div className="mt-[15px] text-[20px] font-[600]">
+              {numberFormatter(reward?.amount, 6, true, {
+                isShort: true,
+                isShortUppercase: true
+              })}{rewardUsd && `(${rewardUsd})`}&nbsp;&nbsp;
+              {reward?.symbol}
+            </div>
+            {/*<div className="mt-[5px] text-[16px] font-[500]">*/}
+            {/*  +{numberFormatter(currentReward?.usd, 2, true, { prefix: "$", isShort: true, isShortUppercase: true })}*/}
+            {/*</div>*/}
           </div>
-          {/*<div className="mt-[5px] text-[16px] font-[500]">*/}
-          {/*  +{numberFormatter(currentReward?.usd, 2, true, { prefix: "$", isShort: true, isShortUppercase: true })}*/}
-          {/*</div>*/}
-        </div>
-      ))}
+        );
+      })}
 
       <button
         type="button"
