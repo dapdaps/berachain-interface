a5be42b264dd6d92a430d57d9d56cd16b18f19fa
jimmygu <jimmyguu2024@gmail.com>
2025-05-13 10:31:18 +0800

    feat: Refactor layout to ensure proper element sizing and spacing
    Added `shrink-0` to maintain fixed element sizes and adjusted padding and overflow for the computer section. These changes enhance the layout's consistency and prevent unexpected resizing or content overflow issues.

--- src/sections/home-earth/mc-bera/index.tsx
+++ src/sections/home-earth/mc-bera/index.tsx
@@ -34,11 +34,11 @@ const McBera = (props: any) => {
         <div className="absolute z-[1] left-0 bottom-[318px] w-[190px] h-[649px] bg-[url('/images/home-earth/mc-bera/bg-window-left.png')] bg-no-repeat bg-left bg-contain"/>
         <div className="absolute z-[1] right-0 bottom-[318px] w-[200px] h-[652px] bg-[url('/images/home-earth/mc-bera/bg-window-right.png')] bg-no-repeat bg-left bg-contain"/>
         <div className="relative z-[2] flex items-end justify-center w-full h-full bg-[url('/images/home-earth/mc-bera/bg-table.png')] bg-no-repeat bg-bottom bg-[length:100%_358px]">
-          <div className="relative z-[1] w-[281px] h-[204px] translate-x-[70px] translate-y-[-40px] bg-[url('/images/home-earth/mc-bera/bg-books.png')] bg-no-repeat bg-bottom bg-contain" />
-          <div className="relative z-[2] w-[1036px] h-[780px] bg-[url('/images/home-earth/mc-bera/bg-computer.png')] bg-no-repeat bg-bottom bg-contain">
+          <div className="relative z-[1] w-[281px] h-[204px] shrink-0 translate-x-[70px] translate-y-[-40px] bg-[url('/images/home-earth/mc-bera/bg-books.png')] bg-no-repeat bg-bottom bg-contain" />
+          <div className="relative z-[2] w-[1036px] h-[780px] shrink-0 p-[20px_20px_124px] overflow-hidden bg-[url('/images/home-earth/mc-bera/bg-computer.png')] bg-no-repeat bg-bottom bg-contain">
             <ChatLayout />
           </div>
-          <div className="relative z-[1] w-[266px] h-[240px] translate-x-[-20px] translate-y-[-50px] bg-[url('/images/home-earth/mc-bera/bg-photo.png')] bg-no-repeat bg-bottom bg-contain" />
+          <div className="relative z-[1] w-[266px] h-[240px] shrink-0 translate-x-[-20px] translate-y-[-50px] bg-[url('/images/home-earth/mc-bera/bg-photo.png')] bg-no-repeat bg-bottom bg-contain" />
         </div>
       </motion.div>
     </div>
