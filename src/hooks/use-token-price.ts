e54e55217934fabaeb7445b41370f5d073f6e6ee
jimmygu <jimmygujh@gmail.com>
2025-07-02 19:28:50 +0800

    fix: remove price api proxy and remove useless price api

--- src/hooks/use-token-price.ts
+++ src/hooks/use-token-price.ts
@@ -8,28 +8,15 @@ export default function () {
   const setPriceStore = usePriceStore((store: any) => store.set);
   const [pending, setPending] = useState(false);
   const initializePrice = useCallback(async () => {
-    console.log("======11111========")
     if (pending) return;
     setPending(true);
-    let beraTownPrice: any = {};
     try {
       const beraTownPriceRes = await http.get(
         `${process.env.NEXT_PUBLIC_API}/api/token/price`
       );
-      if (beraTownPriceRes.code === 0) {
-        beraTownPrice = beraTownPriceRes.data;
-      }
-    } catch (err: any) {
-      console.log('get beratown price failed: %o', err);
-    }
-
-    try {
-      const res = await http.get(
-        "https://api.dapdap.net/get-token-price-by-dapdap"
-      );
       setPriceStore({
-        price: res.data || {},
-        beraTownPrice,
+        price: beraTownPriceRes.data || {},
+        beraTownPrice: beraTownPriceRes.data,
       });
       setTimeout(() => {
         initializePrice();
