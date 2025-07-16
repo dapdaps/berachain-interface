48f00f196236936bbb2b64356a0fd50d9b7417b2
jimmygu <jimmygujh@gmail.com>
2025-07-16 14:42:01 +0800

    feat: add bp to belong entry

--- src/components/vaults/index.tsx
+++ src/components/vaults/index.tsx
@@ -14,6 +14,7 @@ const Vaults = (props: any) => {
         className
       )}
       // data-bp="1022-001-001"
+      data-bp="1010-021"
       onClick={() => {
         // router.push("/vaults");
         router.push("/belong");
