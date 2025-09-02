f3210d6cbfdc327c6d2356014d54d3443367df91
jimmygu <jimmygujh@gmail.com>
2025-09-02 10:31:40 +0800

    feat: add check in modal

--- src/components/check-in/entry.tsx
+++ src/components/check-in/entry.tsx
@@ -3,83 +3,25 @@
 import clsx from "clsx";
 import { motion } from "framer-motion";
 import Popover, { PopoverPlacement, PopoverTrigger } from "../popover";
-import CheckIn, { CheckInDays } from ".";
-import { useRef, useState } from "react";
+import CheckIn from ".";
 import CheckInRewardView from "./reward";
-import { Reward, RewardType } from "./config";
-import useCustomAccount from "@/hooks/use-account";
-import { useRequest } from "ahooks";
-import { cloneDeep } from "lodash";
-import { get, post } from "@/utils/http";
-import { numberFormatter } from "@/utils/number-formatter";
+import { useCheckIn } from "@/hooks/use-check-in";
 
 const CheckInEntry = (props: any) => {
   const { className } = props;
 
-  const { accountWithAk } = useCustomAccount();
-  const checkInRef = useRef<any>();
-
-  const [openReward, setOpenReward] = useState<boolean>(false);
-  const [rewardData, setRewardData] = useState<Reward[]>();
-
-  const { data: checkInData, loading: checkInDataLoading, runAsync: getCheckInData } = useRequest(async (mockCheckDay?: number) => {
-    if (!accountWithAk) {
-      return;
-    }
-    const checkInDays = cloneDeep(CheckInDays);
-    const res = await get("/api/go/checkin");
-    if (res.code !== 200 || !res.data) {
-      return;
-    }
-    const { consecutive_check_in = 0 } = res.data;
-    if (consecutive_check_in > 0) {
-      for (let i = 0; i < (consecutive_check_in % 7 === 0 ? 7 : consecutive_check_in % 7); i++) {
-        checkInDays[i].checked = true;
-      }
-    }
-    const _checkInData = {
-      ...res.data,
-      days: checkInDays,
-    };
-    return _checkInData;
-  }, {
-    refreshDeps: [accountWithAk]
-  });
-
-  const { runAsync: onCheckIn, loading: checkInPending } = useRequest(async () => {
-    if (!accountWithAk) {
-      return;
-    }
-    const res = await post("/api/go/checkin");
-    if (res.code !== 200 || !res.data || res.data.success !== true) {
-      return;
-    }
-    checkInRef.current?.onClose?.();
-    // reload check in data
-    getCheckInData();
-    const { reward_box_amount, reward_gem_amount } = res.data;
-    // popup reward
-    const _rewardData: any = [];
-    if (reward_box_amount) {
-      // _rewardData.push({
-
-      // });
-    }
-    if (reward_gem_amount) {
-      _rewardData.push({
-        type: RewardType.Gem,
-        amount: reward_gem_amount,
-        label: `${numberFormatter(reward_gem_amount, 0, true, { isShort: true, isShortUppercase: true })} Gem`,
-      });
-    }
-    if (_rewardData.length > 0) {
-      setRewardData(_rewardData);
-      setOpenReward(true);
-    }
-    return res.data;
-  }, {
-    manual: true,
-  });
+  const {
+    checkInRef,
+    openReward,
+    rewardData,
+    checkInData,
+    checkInDataLoading,
+    getCheckInData,
+    onCheckIn,
+    checkInPending,
+    setOpenReward,
+    setRewardData,
+  } = useCheckIn();
 
   return (
     <>
---
+++ src/hooks/use-check-in.ts
@@ -0,0 +1,88 @@
+import { CheckInDays } from "@/components/check-in/index";
+import { useRef, useState } from "react";
+import { Reward, RewardType } from "@/components/check-in/config";
+import useCustomAccount from "@/hooks/use-account";
+import { useRequest } from "ahooks";
+import { cloneDeep } from "lodash";
+import { get, post } from "@/utils/http";
+import { numberFormatter } from "@/utils/number-formatter";
+
+export const useCheckIn = () => {
+  const { accountWithAk } = useCustomAccount();
+  const checkInRef = useRef<any>();
+
+  const [openReward, setOpenReward] = useState<boolean>(false);
+  const [rewardData, setRewardData] = useState<Reward[]>();
+
+  const { data: checkInData, loading: checkInDataLoading, runAsync: getCheckInData } = useRequest(async () => {
+    if (!accountWithAk) {
+      return;
+    }
+    const checkInDays = cloneDeep(CheckInDays);
+    const res = await get("/api/go/checkin");
+    if (res.code !== 200 || !res.data) {
+      return;
+    }
+    const { consecutive_check_in = 0 } = res.data;
+    if (consecutive_check_in > 0) {
+      for (let i = 0; i < (consecutive_check_in % 7 === 0 ? 7 : consecutive_check_in % 7); i++) {
+        checkInDays[i].checked = true;
+      }
+    }
+    const _checkInData = {
+      ...res.data,
+      days: checkInDays,
+    };
+    return _checkInData;
+  }, {
+    refreshDeps: [accountWithAk]
+  });
+
+  const { runAsync: onCheckIn, loading: checkInPending } = useRequest(async () => {
+    if (!accountWithAk) {
+      return;
+    }
+    const res = await post("/api/go/checkin");
+    if (res.code !== 200 || !res.data || res.data.success !== true) {
+      return;
+    }
+    checkInRef.current?.onClose?.();
+    // reload check in data
+    getCheckInData();
+    const { reward_box_amount, reward_gem_amount } = res.data;
+    // popup reward
+    const _rewardData: any = [];
+    if (reward_box_amount) {
+      // _rewardData.push({
+
+      // });
+    }
+    if (reward_gem_amount) {
+      _rewardData.push({
+        type: RewardType.Gem,
+        amount: reward_gem_amount,
+        label: `${numberFormatter(reward_gem_amount, 0, true, { isShort: true, isShortUppercase: true })} Gem`,
+      });
+    }
+    if (_rewardData.length > 0) {
+      setRewardData(_rewardData);
+      setOpenReward(true);
+    }
+    return res.data;
+  }, {
+    manual: true,
+  });
+
+  return {
+    checkInRef,
+    openReward,
+    rewardData,
+    checkInData,
+    checkInDataLoading,
+    getCheckInData,
+    onCheckIn,
+    checkInPending,
+    setOpenReward,
+    setRewardData,
+  };
+};
