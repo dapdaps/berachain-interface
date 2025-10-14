56686575eb536020201bdb55808c13e75ddea2ce
jimmygu <jimmygujh@gmail.com>
2025-10-15 01:07:15 +0800

    feat: add new lootbox feature with NFT rewards and update related assets

--- src/components/check-in/config.ts
+++ src/components/check-in/config.ts
@@ -3,10 +3,12 @@ export enum RewardType {
   Gem = "points",
   Box = "box",
   Cosmetic = "cosmetic",
+  NFT = "nft",
 }
 
 export interface Reward {
   type: RewardType;
   amount: number | string;
   label: any;
+  img?: any;
 }
